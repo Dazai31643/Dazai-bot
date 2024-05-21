e36c9f78cf8b5c5ffe647025d8981effe0f2ed32
Dazai31643 <163316742+Dazai31643@users.noreply.github.com>
2024-05-22 01:13:09 +0300

    Update حب.js

--- plugins/حب.js
+++ plugins/حب.js
@@ -1,5 +1,5 @@
 import _translate from "./_translate.js"
-const tradutor = _translate.plugins.fun_love
+const tradutor = _translate.plugins.حب
 // Para configurar o idioma, na raiz do projeto altere o arquivo config.json
 // Para configurar el idioma, en la raíz del proyecto, modifique el archivo config.json.
 // To set the language, in the root of the project, modify the config.json file.

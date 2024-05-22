054f785c9543528e6c4706ab4562be70adfa9e85
Dazai31643 <163316742+Dazai31643@users.noreply.github.com>
2024-05-22 03:00:10 +0300

    Create منع الشتائم.js

---
+++ plugins/منع الشتائم.js
@@ -0,0 +1,36 @@
+// TheMystic-Bot-MD@BrunoSobrino - _antitoxic.js
+import _translate from "./_translate.js"; // Modulo Tradutor
+const tradutor = _translate.plugins._antitoxic // Opção de Tradução do arquivo
+ // Para configurar o idioma, na raiz do projeto altere o arquivo config.json
+  // Para configurar el idioma, en la raíz del proyecto, modifique el archivo config.json.
+  // To set the language, in the root of the project, modify the config.json file.
+
+
+const toxicRegex = /خول|عرص|متناك|كسمك|زب|زبي|قحبه|شرموطه|انيكك|انيك امك/i;
+
+export async function before(m, {isAdmin, isBotAdmin, isOwner}) {
+  if (m.isBaileys && m.fromMe) {
+    return !0;
+  }
+  if (!m.isGroup) {
+    return !1;
+  }
+  const user = global.db.data.users[m.sender];
+  const chat = global.db.data.chats[m.chat];
+  const bot = global.db.data.settings[mconn.conn.user.jid] || {};
+  const isToxic = toxicRegex.exec(m.text);
+
+  if (isToxic && chat.antiToxic && !isOwner && !isAdmin) {
+    user.warn += 1;
+    if (!(user.warn >= 5)) await m.reply(`${tradutor.texto1}` + `${user.warn == 1 ? `@${m.sender.split`@`[0]}` : `@${m.sender.split`@`[0]}`}, ${tradutor.texto1_1}"${isToxic}" ${tradutor.texto1_2} ${user.warn}/5` + '*', false, {mentions: [m.sender]});
+  }
+
+  if (user.warn >= 5) {
+    user.warn = 0;
+    await m.reply(`${tradutor.texto2} @${m.sender.split('@')[0]}, ${tradutor.texto2_1}`, false, {mentions: [m.sender]});
+    user.banned = true;
+    await mconn.conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+    // await this.updateBlockStatus(m.sender, 'block')
+  }
+  return !1;
+}

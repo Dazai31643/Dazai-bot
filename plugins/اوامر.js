3eda0744d968b74c6738e9f900b6dce26cc3e37c
Dazai31643 <163316742+Dazai31643@users.noreply.github.com>
2024-05-23 00:18:38 +0300

    Update اوامر.js

--- plugins/اوامر.js
+++ plugins/اوامر.js
@@ -536,7 +536,7 @@ import {
    
     global.fcontact = { key: { fromMe: false, participant: `0@s.whatsapp.net`, remoteJid: 'status@broadcast' }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     const infoText = `
-    ${botname} あ⁩ 」\n
+   𝐷𝐴𝑍𝐴𝐼-𝐵𝛩𝑇 あ⁩ 」\n
     Hii ${name} Senpai
     
     *${ucpn}* 

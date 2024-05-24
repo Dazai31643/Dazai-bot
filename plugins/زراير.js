bbf675593c31067050747a2bf30cd62cafc484d0
Dazai31643 <163316742+Dazai31643@users.noreply.github.com>
2024-05-24 03:49:44 +0300

    Update زراير.js

--- plugins/زراير.js
+++ plugins/زراير.js
@@ -30,7 +30,17 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
                             header: '👑 قـسـم الـمـطـور',
                             title: '.المطور',
                             description: '',
-                            id: 'المطور'
+                            id: 'function handler(m) {
+  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
+  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
+
+}
+
+handler.help = ['owner']
+handler.tags = ['main']
+handler.command = ['owner', 'مطور', 'المطور', 'المالك'] 
+
+export default handler'
                           }
                         ]
                       }

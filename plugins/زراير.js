0ed8061e7095d458f8f1bef3a44bb72c93d766cc
Dazai31643 <163316742+Dazai31643@users.noreply.github.com>
2024-05-24 03:59:00 +0300

    Update زراير.js

--- plugins/زراير.js
+++ plugins/زراير.js
@@ -36,7 +36,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
                       }
                     ]
                   }),
-                  messageParamsJson: ''
+                  messageParamsJson: '.المطور'
                 }
               ]
             }

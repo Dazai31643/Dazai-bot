6c85f537a4fa317dab41af4b0df073629d8759e5
Dazai31643 <163316742+Dazai31643@users.noreply.github.com>
2024-06-03 02:50:15 +0300

    Update تيك.js

--- plugins/تيك.js
+++ plugins/تيك.js
@@ -1,3 +1,8 @@
+handler.help = ['tiktok']
+handler.tags = ['dl']
+handler.command = /^t(t|iktok(d(own(load(er)?)?|l))?|td(own(load(er)?)?|l))|تيك|تيكتوك$/i
+handler.limit = 1
+
 let handler = async (m, { conn, text, usedPrefix, command, args }) => {
   if (!text) return conn.reply(m.chat, `*عاوز تحميل ايه يحب ؟🤔*\n*ضيف رابط الفديو يحب*\n*مثال:*\n*${usedPrefix + command} https://www.tiktok.com/@ox__zoro__ox?_t=8ggRMe37f9y&_r=1*`, m)
   if (!/(?:https:?\/{2})?(?:w{3}|vm|vt|t)?\.?tiktok.com\/([^\s&]+)/gi.test(text)) return conn.reply(m.chat, `*رابط التيكتوك غير صحيح*`, m)
@@ -8,13 +13,8 @@ let handler = async (m, { conn, text, usedPrefix, command, args }) => {
      .catch(async _ => await tiktokdlv3(args[0]))
     const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd
     if (!url) return conn.reply(m.chat, `*اوووف, خطأ أثناء محاولة تنزيل الفيديو ، يرجى المحاولة مرة أخرى*`, m)
-    conn.sendFile(m.chat, url, 'tiktok.mp4', `*تمت المهمة* 🫡💚`.trim(), m)
+    // هنا هو المكان الذي يجب أن ترسل الملف من هنا
+    conn.sendMessage(m.chat, { video: { url: url }, caption: `*تمت المهمة* 🫡💚`.trim() }, { quoted: m })
   } catch {
   }
 }
-
-handler.help = ['tiktok']
-handler.tags = ['dl']
-handler.command = /^t(t|iktok(d(own(load(er)?)?|l))?|td(own(load(er)?)?|l))|تيك|تيكتوك$/i
-handler.limit = 1
-export default handler
